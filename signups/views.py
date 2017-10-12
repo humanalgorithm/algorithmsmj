804c55cf21585b508c854c63513e01849c06e426
humanalgorithm <scottomj@gmail.com>
2017-10-12 01:05:53 -0400

    working signsups, forms and catch null set error

--- signups/views.py
+++ signups/views.py
@@ -1,22 +1,39 @@
-from django.shortcuts import  render, HttpResponseRedirect
-from .forms import SignUpForm
+from django.shortcuts import render, HttpResponseRedirect
 from rest_framework import generics
 from models import SignUp
 from serializers import SignUpSerializer
 from mail import MailSender
+from email.utils import parseaddr
+from django.views.generic.base import View
+from .models import SignUp
+from django.contrib import messages
+
+class SignUpView(View):
+    def post(self, request):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        print request.POST
+
+        if first_name and last_name and self.valid_email(email):
+            SignUp.objects.create(first_name=first_name, last_name=last_name, email=email)
+            MailSender().send_mail_to_signup(request, first_name, email)
+            messages.success(request, 'You have successfully signed up and will receive an email.')
+            return render(request, "page/thankyou.html")
+        else:
+            if not first_name:
+                messages.error(request, "Please provide a first name")
+            if not last_name:
+                messages.error(request, "Please provide a last name")
+            if not self.valid_email(email):
+                messages.error(request, "Please provide a valid email")
+            return render(request, "page/thankyou.html")
 
 
-def signup(request):
-  form = SignUpForm(request.POST or None)
-  MailSender().send_mail_to_signup(form, request)
-  return HttpResponseRedirect('/thank-you/')
-
-def thankyou(request):
-    return render(request, "thankyou.html")
-
+    def valid_email(self, email):
+        return '@' in parseaddr(email)[1]
 
 
 class SignUpList(generics.ListCreateAPIView):
     queryset = SignUp.objects.all()
     serializer_class = SignUpSerializer
-
--- urls.py
+++ urls.py
@@ -4,17 +4,9 @@ from django.conf.urls import include
 from django.contrib import admin
 
 admin.autodiscover()
-
-import homepage.urls
-
-
-
 urlpatterns = [
                url(r'^admin/', admin.site.urls),
                url(r'', include('sorting.urls')),
-               url(r'', include('homepage.urls')),
                url(r'', include('signups.urls')),
+               url(r'', include('homepage.urls')),
                ]
-
-
-#urlpatterns = sorting_urlpatterns
